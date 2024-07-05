217b00b89bef1f13a597feb1dc7c4a837b5e82d0
Sergio Rey <reysergio383@gmail.com>
2024-07-05 17:37:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <section className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center flex flex-col items-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">Makers website is being updated</h1>
+        <img className="size-52 my-7 rounded-lg" src="../public/work-penguin.gif" alt="" />
+        
+        <p className="text-2xl mb-4 text-gray-800 font-bold">Although the site is still under construction, everybody can still apply to become part of the <span className="text-purple-500">Makers</span> community</p>
+        
+        <a className="text-2xl font-bold text-green-500 hover:text-purple-500 duration-200 block mx-auto" href="https://airtable.com/appV1aT1ZYuXvXANu/shra5fYnJ8r0XwKJp">Apply Here!</a>
+      </div>
+    </section>
+  );
+}
+
+export default App;
